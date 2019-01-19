cb735d5a9ba470c812b3f37f63fa4bc6c444f5ef
Teddy Mabulay <temabu@ttu.ee>
2019-01-19 19:42:45 +0200

    2. All ones game

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -19,7 +19,13 @@ defmodule Bowling do
   def score(game) do
     game
     |> List.flatten
-    |> Enum.filter(fn e -> is_number e end)
-    |> Enum.sum
+
+    |> Enum.filter(&(is_number(&1)))
+    # |> Enum.filter(fn e -> is_number e end)
+    # |> Enum.filter(&is_number/1)
+
+    |> Enum.reduce(&+/2)
+    # |> Enum.reduce(0, fn n, acc -> n + acc end)
+    # |> Enum.sum
   end
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -10,4 +10,10 @@ defmodule BowlingTest do
     game = List.duplicate([0, 0], 9) ++ [[0, 0, nil]]
     assert Bowling.score(game) == 0
   end
+
+  test "'all ones' game" do
+    game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
+    assert Bowling.score(game) == 20
+  end  
+  
 end
