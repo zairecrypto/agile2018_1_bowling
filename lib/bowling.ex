4be58967af2816963254ad7cd3f10508cec39d78
Teddy Mabulay <temabu@ttu.ee>
2019-01-19 20:02:35 +0200

    all done

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -16,16 +16,43 @@ defmodule Bowling do
     :world
   end
 
+
   def score(game) do
-    game
-    |> List.flatten
+  # VERSION 1 {
 
-    |> Enum.filter(&(is_number(&1)))
+    # game
+    # |> List.flatten
+    
     # |> Enum.filter(fn e -> is_number e end)
+    # |> Enum.filter(&(is_number(&1)))
     # |> Enum.filter(&is_number/1)
-
-    |> Enum.reduce(&+/2)
+    
     # |> Enum.reduce(0, fn n, acc -> n + acc end)
+    # |> Enum.reduce(&+/2)
     # |> Enum.sum
+
+  # }
+
+  # VERSION 2 {
+    game_list = List.flatten(game)
+    |> Enum.map(fn x when is_nil(x) -> 0; x-> x end)
+
+    _score(game_list ++ [:end])
+  # }
   end
+
+  defp _score([10,a2,b1,:end]), do: 10 + a2 + b1
+
+  #two strike
+  defp _score([10,_,10,b2,c1 | tail]), do: 20 + c1 + _score([10,b2,c1|tail])
+
+  # one strike
+  defp _score([10,_,b1,b2 | tail]), do: 10 + b1 + b2 + _score([b1,b2|tail])
+
+  # one spare
+  defp _score([a1,a2,b1 | tail]) when a1 + a2 == 10, do: 10 + b1 + _score([b1|tail])
+  
+  # default scoring
+  defp _score([a1,:end]), do: a1
+  defp _score([a1,a2 | tail]), do: a1 + a2 + _score(tail)
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -15,5 +15,19 @@ defmodule BowlingTest do
     game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
     assert Bowling.score(game) == 20
   end  
-  
+
+  test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 16
+  end
+
+  test "one strike" do
+    game = [[10,nil],[3,4]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 24
+  end
+
+  test "perfect game" do
+    game = List.duplicate([10,nil], 9) ++ [[10,10,10]]
+    assert Bowling.score(game) == 300    
+  end
 end
